c1387b4b90701c9e180aff0ed42b8fa0b7f24231
Tombamac <55087826+Tombamac@users.noreply.github.com>
2019-11-03 14:44:32 +0000

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,117 @@
+/**
+ * An implemetaiton of a card type.
+ * 
+ * @author 05018728
+ *
+ */
+public class Card {
+
+		/**
+		 * One of the four valid suits for this card
+		 */
+	private Suit mySuit;
+	
+	/**
+	 * the number of this card, where Ace: 1, Jack - King : 11 - 13
+	 */
+	private int myNumber;
+	
+	
+	/**
+	 * The card constructor
+	 * 
+	 */
+	public Card(Suit aSuit, int aNumber)
+	{
+		this.mySuit = aSuit;
+		
+		if (aNumber >=1 && aNumber <= 13)
+		{
+			this.myNumber = aNumber;
+		}
+		
+		else
+		{
+			System.err.println(aNumber + " is not a valid card number");
+			System.exit(1);
+		}
+			
+		
+	}
+	
+	/**
+	 * Return the number of the card.
+	 * 
+	 * @return the number
+	 */
+	public int getNumber()
+	{
+		return myNumber;
+	}
+	
+	public String toString()
+	{
+		String numStr = "Error";
+		
+		switch(this.myNumber)
+		{
+		
+		case 2:
+			numStr = "Two";
+			break;
+			
+		case 3:
+			numStr = "Three";
+			break;
+			
+		case 4:
+			numStr = "Four";
+			break;
+			
+		case 5:
+			numStr = "Five";
+			break;
+			
+		case 6:
+			numStr = "Six";
+			break;
+			
+		case 7:
+			numStr = "Seven";
+			break;
+			
+		case 8:
+			numStr = "Eight";
+			break;
+			
+		case 9:
+			numStr = "Nine";
+			break;
+			
+		case 10:
+			numStr = "Ten";
+			break;
+			
+		case 11:
+			numStr = "Jack";
+			break;
+			
+		case 12:
+			numStr = "Queen";
+			break;
+			
+		case 13:
+			numStr = "King";
+			break;
+			
+		case 1:
+			numStr = "Ace";
+			break;
+				
+		}
+		
+		return numStr + " of " + mySuit.toString();
+		
+	}
+	
+} //End of class
---
+++ Suit.java
@@ -0,0 +1,12 @@
+/**
+ * An enum representing the four possible suits of a card.
+ * @author 05018728
+ *
+ */
+public enum Suit
+{
+	Clubs,
+	Diamonds,
+	Spades,
+	Hearts,
+}
